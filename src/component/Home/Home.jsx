9d00ceacb1932041fcaf358e9a8c837e8d5dc679
Irwan333 <57896857+Irwan333@users.noreply.github.com>
2019-11-20 17:48:01 +0700

    mengupload latihan reactjs 2
    latihan reactjs menggunakan bootstrap

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+class Home extends Component{
+render(){
+return(
+<div className="slider">
+    <div className="sliderimage">
+        <div className="row  justify-content-center flex-row">
+            <img className="rounded" src={require('../../gambar/desktop.jpg')} alt="Responsive image" />
+        </div>
+    </div>
+    <div className="notive">
+        <div className="row">
+            <div className="col-md-12">
+                <div className="alert alert-danger danger" role="alert">
+                    <p className="text-white text-center">A simple success alert-check it out!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className="slide">
+        <div className="row">
+            <div className="col-md-9">
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={require('../../gambar/slide1.jpg')} className="img-fluid d-block w-100 tinggi"
+                                alt="..." />
+                        </div>
+                        <div className="carousel-item active">
+                            <img src={require('../../gambar/slide2.jpeg')} className="img-fluid d-block w-100 tinggi"
+                                alt="..." />
+                        </div>
+                        <div className="carousel-item active">
+                            <img src={require('../../gambar/slide3.jpg')} className="img-fluid d-block w-100 tinggi"
+                                alt="..." />
+                        </div>
+                    </div>
+                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                        data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button"
+                        data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            <div className="col-md-3">
+                <img className="img-fluid rounded slide" src={require('../../gambar/mini-banner-1-raw.png')}
+                    alt="Responsive image" />
+                <img className="img-fluid rounded slide" src={require('../../gambar/mini-banner-2-raw.png')}
+                    alt="Responsive image" />
+            </div>
+        </div>
+    </div>
+</div>
+)
+}
+}
+
+export default Home;
